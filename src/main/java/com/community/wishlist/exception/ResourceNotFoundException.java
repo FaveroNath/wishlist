816be9bb750e5d12c6fa73652bb5634d34c65b40
Nathalia Favero <faveronathalia@gmail.com>
2023-03-04 13:25:19 -0300

    Add exception handling

---
+++ src/main/java/com/community/wishlist/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.community.wishlist.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
